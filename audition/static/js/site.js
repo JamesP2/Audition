0dac901321cdc53de1fd3ad4d4215f914c19722c
James Phillips <james@thefailboat.co.uk>
2017-01-23 22:15:46 +0000

    Markdown help template

---
+++ audition/static/js/site.js
@@ -0,0 +1,8 @@
+function popup(mylink, windowname, width, height) {
+    if (!window.focus)return true;
+    var href;
+    if (typeof(mylink) == 'string') href = mylink;
+    else href = mylink.href;
+    window.open(href, windowname, 'width=' + width + ',height=' + height + ',scrollbars=yes');
+    return false;
+}
